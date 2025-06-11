f58c73f855c85e6be42fcd7912be7348e762e704
yoskirukato <yoskirukato@gmail.com>
2025-06-11 17:01:48 +0300

    Refactor webhook URL definition to be set after WordPress initialization

--- includes/config.php
+++ includes/config.php
@@ -6,5 +6,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Telegram Bot Configuration
  define( 'TBA_BOT_TOKEN', get_option( 'tba_bot_token', '' ) );
  define( 'TBA_BOT_USERNAME', get_option( 'tba_bot_username', '' ) );
- define( 'TBA_WEBHOOK_URL', rest_url( 'tba/v1/webhook' ) );
+
+// Webhook URL will be defined after WordPress is fully loaded
+function tba_define_webhook_url() {
+    if ( ! defined( 'TBA_WEBHOOK_URL' ) ) {
+        define( 'TBA_WEBHOOK_URL', rest_url( 'tba/v1/webhook' ) );
+    }
+}
+add_action( 'init', 'tba_define_webhook_url' );
 ?> 
